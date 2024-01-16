968eeb3ba08d77cc3ba05db53db4706ec63b018e
Tej Bari <136843098+tejbari5@users.noreply.github.com>
2024-01-16 10:17:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const candidateSwitchCheckbox = document.getElementById('candidate-switch'); let selectedCell;
+document.querySelectorAll('td:not(.given-number)').forEach(cell => cell.addEventListener('click', () => { if (selectedCell) selectedCell.classList.remove('selected'); selectedCell = cell;
+selectedCell.classList.add('selected')
+}))
+document.querySelectorAll('.number-control').forEach(button => {
+button.addEventListener('click', () => {
+if (!selectedCell) return;
+if (candidateSwitchCheckbox.checked) {
+let candidatesArray = selectedCell.querySelector('.candidates').textContent.split('');
+if (candidatesArray.includes (button.textContent)) {
+candidatesArray.splice(candidatesArray.indexOf(button.textContent), 1);
+} else {
+candidatesArray = [...candidatesArray, button.textContent].sort(); }
+selectedCell.querySelector('.candidates').textContent = candidatesArray.join('');
+} else {
+selectedCell.querySelector('.value').textContent = button.textContent;
+}
+})
+})
